8dc927ec45684b7e42a478825898ae7691291370
gauravanthwal <gauravanthwal7409@gmail.cm>
2023-12-09 19:00:38 +0530

    cart initial load fixed

--- src/config/fetchConfig.ts
+++ src/config/fetchConfig.ts
@@ -3,23 +3,19 @@ import { getFromStorage } from "./storageConfig";
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1`;
 
-const getHeaders = () =>{
-    if(getFromStorage("accessToken")){
-        return getFromStorage("accessToken")
-    }
-}
+
 
 export const axiosClient = axios.create({
-    baseURL: BASE_URL, 
-    // headers: {
-    //     Authorization: `Bearer ${'token'}`
-    // }
-})
+  baseURL: BASE_URL,
+  // headers: {
+  //     Authorization: `Bearer ${'token'}`
+  // }
+});
 
 
 export const axiosClientWithHeaders = axios.create({
-    baseURL: BASE_URL, 
-    headers: {
-        Authorization: `Bearer ${getHeaders()}`
-    }
-})
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${getFromStorage("accessToken")}`,
+  },
+});
